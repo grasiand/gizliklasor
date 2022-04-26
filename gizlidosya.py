fcb185fbf5143aa7cf8743eefda5b0355f05321c
Grasiand <87449297+grasiand@users.noreply.github.com>
2022-04-26 13:53:21 +0300

    Update gizlidosya.py

--- gizlidosya.py
+++ gizlidosya.py
@@ -7,8 +7,8 @@ KULLANICI GİRİŞ EKRANI
 
 """)
 
-sys_username = "Grasiand"
-sys_password = "Grasiand"
+sys_username = "istediginiz kullanici adi"
+sys_password = "istediginiz sifre"
 
 kullanici_adi = input("Kullanıcı Adını Giriniz: ")
 sifre = input("Şifre'yi Giriniz: ")
@@ -23,4 +23,4 @@ elif (kullanici_adi != sys_username) and (sifre != sys_password):
     print("Kullanıcı adı ve şifre yanlış..")
 else:
     print("Giriş yapıldı!")
-    os.system("start C:\\Users\\Kullanıcı ismi\\gizlediğiniz klasor ismi")
+    os.system("start C:\\Users\\kullanici ismi\\gizlediğiniz klasor ismi")
