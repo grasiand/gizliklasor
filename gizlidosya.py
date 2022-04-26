a4daf22c783da3885a06d0d5fc0e9d05685146f8
Grasiand <87449297+grasiand@users.noreply.github.com>
2022-04-26 13:52:49 +0300

    Add files via upload

---
+++ gizlidosya.py
@@ -0,0 +1,26 @@
+import os
+author == Grasiand
+print("""
+
+KULLANICI GİRİŞ EKRANI
+(Grasiand)
+
+""")
+
+sys_username = "Grasiand"
+sys_password = "Grasiand"
+
+kullanici_adi = input("Kullanıcı Adını Giriniz: ")
+sifre = input("Şifre'yi Giriniz: ")
+
+if (kullanici_adi == sys_username) and (sifre != sys_password):
+    print("Şifre yanlış..")
+
+elif (kullanici_adi != sys_username) and (sifre == sys_password):
+    print("Kullanıcı adı yanlış..")
+
+elif (kullanici_adi != sys_username) and (sifre != sys_password):
+    print("Kullanıcı adı ve şifre yanlış..")
+else:
+    print("Giriş yapıldı!")
+    os.system("start C:\\Users\\Kullanıcı ismi\\gizlediğiniz klasor ismi")
